d3e4de576ca2b0f6c78807292953752879e0036b
6304673J8 <petergeorghe.julca@enti.cat>
2021-04-27 14:32:29 +0200

    New chat configuration on webpage

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const node_static = require("node-static");
 const mongo = require("mongodb").MongoClient;
 
 let server_url = "mongodb://localhost:27017"
-
+let chat_db;
 mongo.connect(server_url, (err, server) => {
 	if(err){
 		console.log("Error connecting to MongoDB");
@@ -14,17 +14,33 @@ mongo.connect(server_url, (err, server) => {
 	}
 	console.log("Into MONGODB Server");
 
-	let chat_db = server.db("amongmeme");
+	chat_db = server.db("amongmeme");
 
-	chat_db.collection("chat").find({});
+	//chat_db.collection("chat").find({});
 });
 
 console.log("INIT CHAT SERVER");
-/*
+
 let public_files = new node_static.Server("pub");
 
-http.createServer((request, response) => {
+http.createServer( (request, response) => {
 	console.log("File"+request.url);
+	if (request.url == "/chat"){
+		console.log("Chat Starting");
+		let cursor = chat_db.collection("chat").find({});
+
+		let chat = cursor.toArray();		
+
+		chat.then( (data) =>{
+			console.log(data);
+
+			response.writeHead(200, {'Content-Type': 'text/plain'});
+			response.write( JSON.stringify(data) );
+			response.end();
+		});
+
+		return;
+	}
 	public_files.serve(request, response);
 }).listen(8282);
-*/
+
