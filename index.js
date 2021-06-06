33434758e48d085ac4d990726eba0fa81a6d717d
6304673J8 <petergeorghe.julca@enti.cat>
2021-06-07 00:06:28 +0200

    AMONGCHAT

--- index.js
+++ index.js
@@ -29,7 +29,9 @@ http.createServer( (request, response) => {
 	if (request.url.startsWith("/chat")){
 		let info = request.url.split("=");
 		console.log(info[1]);
-
+		let query = {
+			date: { $gt : parseInt(info[1])}
+		};
 		let cursor = chat_db.collection("chat").find(query);
 		cursor.toArray().then( (data) => {
 			response.writeHead(200, {'Content-Type': 'text/plain'});
@@ -47,14 +49,70 @@ http.createServer( (request, response) => {
 			console.log(count);
 
 			const MAX = 5;
+
+			let cursor = chat_db.collection("chat").find({}, {
+						skip: count - MAX,
+						limit: MAX,
+						sort: { $natural:1 }
+					});
+		cursor.toArray().then( (data) => {
+
+				response.writeHead(200, {'Content-Type': 'text/plain'});			
+			
+				response.write( JSON.stringify(data) );
+
+				response.end();
+			});
+		});	
+		return;
+	}
+	if (request.url == "/submit"){
+		let body = [];
+		console.log("Data transfer...");
+		
+		request.on('data', (chunk) => {
+			body.push(chunk);
+		}).on('end', () => {
+			
+			let chat_data = JSON.parse(Buffer.concat(body).toString());
+
+			chat_db.collection("chat").insertOne({
+				user: chat_data.chat_user,
+				msg: chat_data.chat_msg,
+				date: Date.now()
+			});			 
+
 		});
-	
-		}
 
 		response.end();
+
 		return;
 	}
 
+	if(request.url == "/history")
+	{
+		response.writeHead(200,{"Content-Type":"text/plain"});
+		let cursor = chat_db.collection("chat").find().sort({ date: -1 });
+	
+		let content ="";
+		let message = cursor.toArray();
+
+		message.then((data) =>
+		{
+			for(let i = 0; i<data.length; i++)
+			{
+				let date = data[i].date;
+				date = Number(date);
+				date = new Date(date);
+				let plainDate = date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear() + "  " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+				content = "(" + plainDate + ")" + data[i].user + " : " + data[i].msg;
+
+				response.write(JSON.stringify(content) + "\n" );
+			}
+			response.end();
+		});
+		return;
+	}
 	public_files.serve(request, response);
 }).listen(8282);
 
